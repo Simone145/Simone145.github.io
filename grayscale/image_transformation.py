857d1df961995615a9cd6ad51988edeb78e18d61
Simone145 <49592739+Simone145@users.noreply.github.com>
2019-04-13 16:05:51 -0400

    graypuppies
    puppies turned gray need to change color again

---
+++ grayscale/image_transformation.py
@@ -0,0 +1,21 @@
+
+
+from PIL import Image
+
+my_image = Image.open("pup.jpg")
+image_pixels = my_image.load()
+width, height  = my_image.size
+for i in range(0, width):
+    for j in range(0, height):
+        current_pixel = image_pixels[i,j]
+        blue_component = image_pixels[i,j][0]
+        green_component = image_pixels[i,j][1]
+        red_component = image_pixels[i,j][2]
+        gray_value = (int)(0.07 * blue_component + 0.72 * green_component + 0.21 * red_component)
+        image_pixels[i,j] = (gray_value, gray_value, gray_value, 255)
+        if(gray_value < 100):
+	           new_color = (0, 31, 59, 255)
+        elif (gray_value < 200):
+	           new_color = (215, 30, 1, 255)
+               
+my_image.show()
